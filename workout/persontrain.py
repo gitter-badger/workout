56fe331d7f284cbaace12b2327b2fee370bb332d
jadermcs <jader.mad@gmail.com>
2014-12-16 15:02:29 -0200

    updating functions

--- workout/persontrain.py
+++ workout/persontrain.py
@@ -7,24 +7,16 @@ class Work(object):
 		self.tipo = tipo
 		self.modo = modo
 
-	def trainer(self,tipo):
-		peito=['supino','crucifixo','remada','pullover']
-		triceps=['francesa','pulldown','testa']
-		biceps=['scott', 'concentrada']
-		costas=['puxada']
-		ombro=[]
-		perna=[]
-		abdomen=[]
-
 
 def traingen():
 	tipo=raw_input("Qual tipo de treino?(ABC, AB ou A)")
+	tipo=tipo.upper()
 	modo=raw_input("Acompanhado ou solo?(A ou S)")
+	modo=modo.upper()
+	print tipo, modo
 	tabela()
 	print 'treino aqui'
 	tabela()
-
-traingen()
 
 
 
--- workout/recursos.py
+++ workout/recursos.py
@@ -1,2 +1,10 @@
 def tabela():
 	print "____________"
+
+def trainer(self,tipo):
+	peito=['supino','crucifixo','remada','pullover']
+	triceps=['francesa','pulldown','testa']
+	biceps=['scott', 'concentrada']
+	ombro=[]
+	perna=[]
+	abdomen=[]
